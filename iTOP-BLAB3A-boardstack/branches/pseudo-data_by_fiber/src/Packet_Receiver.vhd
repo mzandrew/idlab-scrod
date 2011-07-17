69ef3a1ff6bea1990c92b0a668a171f6e289ffe6
mzandrew <mzandrew@gmail.com>
2011-07-17 10:23:21 +0000

    fixed bug where the command processor needs more data coming in to process the previously received command - added new state machine to do command processing; this version sucessfully parses packets (but does nothing with the command)

--- iTOP-BLAB3A-boardstack/branches/pseudo-data_by_fiber/src/Packet_Receiver.vhd
+++ iTOP-BLAB3A-boardstack/branches/pseudo-data_by_fiber/src/Packet_Receiver.vhd
@@ -44,7 +44,8 @@ entity Packet_Receiver is
 end Packet_Receiver;
 
 architecture Behavioral of Packet_Receiver is
-	type RECEIVE_STATE_TYPE is (WAITING_FOR_HEADER, READING_PACKET_SIZE, READING_PROTOCOL_DATE, READING_PACKET_TYPE, READING_VALUES, READING_SCROD_REV_AND_ID, READING_CHECKSUM, READING_FOOTER, SET_SEND_EVENT_FLAG, WAITING_FOR_ACKNOWLEDGE);
+	type PACKET_RECEIVER_STATE_TYPE is (WAITING_FOR_HEADER, READING_PACKET_SIZE, READING_PROTOCOL_DATE, READING_PACKET_TYPE, READING_VALUES, READING_SCROD_REV_AND_ID, READING_CHECKSUM, READING_FOOTER);
+	type COMMAND_PROCESSING_STATE_TYPE is (WAITING_TO_PROCESS_COMMAND, SET_SEND_EVENT_FLAG, WAITING_FOR_ACKNOWLEDGE);
 	signal internal_RX_D : std_logic_vector(31 downto 0);
 	signal internal_RX_SRC_RDY_N : std_logic;
 	signal internal_WRONG_PACKET_SIZE_COUNTER          : std_logic_vector(31 downto 0);
@@ -59,7 +60,8 @@ architecture Behavioral of Packet_Receiver is
 	signal internal_resynchronizing_with_header : std_logic;
 	signal internal_start_event_transfer : std_logic;
 	signal internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR : std_logic_vector(31 downto 0);
-	signal RECEIVE_STATE : RECEIVE_STATE_TYPE := WAITING_FOR_HEADER;
+	signal PACKET_RECEIVER_STATE : PACKET_RECEIVER_STATE_TYPE := WAITING_FOR_HEADER;
+	signal COMMAND_PROCESSING_STATE : COMMAND_PROCESSING_STATE_TYPE := WAITING_TO_PROCESS_COMMAND;
 begin
 	internal_RX_D         <= RX_D;
 	internal_RX_SRC_RDY_N <= RX_SRC_RDY_N;
@@ -103,115 +105,127 @@ begin
 			internal_number_of_sent_events         <= (others => '0');
 			internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR <= (others => '0');
 			internal_MISSING_ACKNOWLEDGEMENT_COUNTER <= (others => '0');
-		elsif (rising_edge(USER_CLK) and internal_RX_SRC_RDY_N = '0') then
-			internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR <= std_logic_vector(unsigned(internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR) + 1);
-			case RECEIVE_STATE is
-				when WAITING_FOR_HEADER =>
---					values_read := 0;
-					if (internal_RX_D = x"00BE11E2") then
-						checksum := x"00BE11E2";
-						internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR <= x"00000001";
-						RECEIVE_STATE <= READING_PACKET_SIZE;
-						internal_resynchronizing_with_header <= '0';
-					else
-						internal_resynchronizing_with_header <= '1';
-					end if;
-				when READING_PACKET_SIZE =>
-					packet_size := unsigned(internal_RX_D(15 downto 0));
-					checksum := checksum + packet_size;
-					remaining_words_in_packet := packet_size - 2; -- 1 each for header & packet size
-					if (packet_size = EXPECTED_PACKET_SIZE) then
-						RECEIVE_STATE <= READING_PROTOCOL_DATE;
-					else
-						internal_WRONG_PACKET_SIZE_COUNTER <= std_logic_vector(unsigned(internal_WRONG_PACKET_SIZE_COUNTER) + 1);
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					end if;
-				when READING_PROTOCOL_DATE =>
-					protocol_date := unsigned(internal_RX_D);
-					checksum := checksum + protocol_date;
-					remaining_words_in_packet := remaining_words_in_packet - 1;
-					if (protocol_date = x"20110708") then
-						RECEIVE_STATE <= READING_PACKET_TYPE;
-					else
-						internal_WRONG_PROTOCOL_FREEZE_DATE_COUNTER <= std_logic_vector(unsigned(internal_WRONG_PROTOCOL_FREEZE_DATE_COUNTER) + 1);
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					end if;
-				when READING_PACKET_TYPE =>
-					checksum := checksum + unsigned(internal_RX_D);
-					remaining_words_in_packet := remaining_words_in_packet - 1;
-					if (internal_RX_D = x"B01DFACE") then -- command packet
-						RECEIVE_STATE <= READING_VALUES;
-					else
-						internal_WRONG_PACKET_TYPE_COUNTER <= std_logic_vector(unsigned(internal_WRONG_PACKET_TYPE_COUNTER) + 1);
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					end if;
-				when READING_VALUES =>
-					value := unsigned(internal_RX_D);
-					checksum := checksum + value;
-					remaining_words_in_packet := remaining_words_in_packet - 1;
-					-- ignore stream of values other than to perform checksum
-					if (remaining_words_in_packet = 3) then -- 1 each for SCROD rev/id, checksum and footer
-						RECEIVE_STATE <= READING_SCROD_REV_AND_ID;
-					end if;
-				when READING_SCROD_REV_AND_ID =>
-					revision_and_id := unsigned(internal_RX_D);
-					revision := revision_and_id(31 downto 16);
-					id := revision_and_id(15 downto 0);
-					checksum := checksum + revision_and_id;
-					remaining_words_in_packet := remaining_words_in_packet - 1; -- should be two after this
-					if (id = 0 or id = SCROD_ID) then
-					else
-						internal_WRONG_SCROD_ADDRESSED_COUNTER <= std_logic_vector(unsigned(internal_WRONG_SCROD_ADDRESSED_COUNTER) + 1);
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					end if;
-					if (revision = 0 or revision = SCROD_REVISION) then
-					else
-						internal_WRONG_SCROD_ADDRESSED_COUNTER <= std_logic_vector(unsigned(internal_WRONG_SCROD_ADDRESSED_COUNTER) + 1);
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					end if;
-					RECEIVE_STATE <= READING_CHECKSUM;
-				when READING_CHECKSUM =>
-					-- this state does not change the running checksum "checksum" like all other states do
-					checksum_from_packet := unsigned(internal_RX_D);
-					remaining_words_in_packet := remaining_words_in_packet - 1; -- should be one after this
-					RECEIVE_STATE <= READING_FOOTER;
-				when READING_FOOTER =>
-					footer := unsigned(internal_RX_D);
-					checksum := checksum + footer;
-					remaining_words_in_packet := remaining_words_in_packet - 1; -- should be zero after this
---					if (remaining_words_in_packet = '0') then
---							internal_WRONG__COUNTER <= std_logic_vector(unsigned(internal_WRONG__COUNTER) + 1);
---					end if;
-					if (footer = x"62504944") then
-						if (checksum = checksum_from_packet) then
-							RECEIVE_STATE <= SET_SEND_EVENT_FLAG;
+		elsif (rising_edge(USER_CLK)) then
+			-- this only receives packets when internal_RX_SRC_RDY_N = '0'
+			-- and it only processes commands when internal_RX_SRC_RDY_N = '1'
+			if (internal_RX_SRC_RDY_N = '0') then
+				internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR <= std_logic_vector(unsigned(internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR) + 1);
+				case PACKET_RECEIVER_STATE is
+					when WAITING_FOR_HEADER =>
+--						values_read := 0;
+						if (internal_RX_D = x"00BE11E2") then
+							checksum := x"00BE11E2";
+							internal_NUMBER_OF_WORDS_IN_THIS_PACKET_RECEIVED_SO_FAR <= x"00000001";
+							PACKET_RECEIVER_STATE <= READING_PACKET_SIZE;
+							internal_resynchronizing_with_header <= '0';
 						else
-							internal_WRONG_CHECKSUM_COUNTER <= std_logic_vector(unsigned(internal_WRONG_CHECKSUM_COUNTER) + 1);
-							RECEIVE_STATE <= WAITING_FOR_HEADER;
+							internal_resynchronizing_with_header <= '1';
 						end if;
+					when READING_PACKET_SIZE =>
+						packet_size := unsigned(internal_RX_D(15 downto 0));
+						checksum := checksum + packet_size;
+						remaining_words_in_packet := packet_size - 2; -- 1 each for header & packet size
+						if (packet_size = EXPECTED_PACKET_SIZE) then
+							PACKET_RECEIVER_STATE <= READING_PROTOCOL_DATE;
+						else
+							internal_WRONG_PACKET_SIZE_COUNTER <= std_logic_vector(unsigned(internal_WRONG_PACKET_SIZE_COUNTER) + 1);
+							PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+						end if;
+					when READING_PROTOCOL_DATE =>
+						protocol_date := unsigned(internal_RX_D);
+						checksum := checksum + protocol_date;
+						remaining_words_in_packet := remaining_words_in_packet - 1;
+						if (protocol_date = x"20110708") then
+							PACKET_RECEIVER_STATE <= READING_PACKET_TYPE;
+						else
+							internal_WRONG_PROTOCOL_FREEZE_DATE_COUNTER <= std_logic_vector(unsigned(internal_WRONG_PROTOCOL_FREEZE_DATE_COUNTER) + 1);
+							PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+						end if;
+					when READING_PACKET_TYPE =>
+						checksum := checksum + unsigned(internal_RX_D);
+						remaining_words_in_packet := remaining_words_in_packet - 1;
+						if (internal_RX_D = x"B01DFACE") then -- command packet
+							PACKET_RECEIVER_STATE <= READING_VALUES;
+						else
+							internal_WRONG_PACKET_TYPE_COUNTER <= std_logic_vector(unsigned(internal_WRONG_PACKET_TYPE_COUNTER) + 1);
+							PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+						end if;
+					when READING_VALUES =>
+						value := unsigned(internal_RX_D);
+						checksum := checksum + value;
+						remaining_words_in_packet := remaining_words_in_packet - 1;
+						-- ignore stream of values other than to perform checksum
+						if (remaining_words_in_packet = 3) then -- 1 each for SCROD rev/id, checksum and footer
+							PACKET_RECEIVER_STATE <= READING_SCROD_REV_AND_ID;
+						end if;
+					when READING_SCROD_REV_AND_ID =>
+						revision_and_id := unsigned(internal_RX_D);
+						revision := revision_and_id(31 downto 16);
+						id := revision_and_id(15 downto 0);
+						checksum := checksum + revision_and_id;
+						remaining_words_in_packet := remaining_words_in_packet - 1; -- should be two after this
+						if (id = 0 or id = SCROD_ID) then
+						else
+							internal_WRONG_SCROD_ADDRESSED_COUNTER <= std_logic_vector(unsigned(internal_WRONG_SCROD_ADDRESSED_COUNTER) + 1);
+							PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+						end if;
+						if (revision = 0 or revision = SCROD_REVISION) then
 					else
-						internal_WRONG_FOOTER_COUNTER <= std_logic_vector(unsigned(internal_WRONG_FOOTER_COUNTER) + 1);
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					end if;
-				when SET_SEND_EVENT_FLAG =>
-					internal_number_of_sent_events <= std_logic_vector(unsigned(internal_number_of_sent_events) + 1);
-					internal_start_event_transfer <= '1';
-					timeout_waiting_for_acknowledge_counter := NUMBER_OF_CYCLES_TO_WAIT_FOR_ACKNOWLEDGE;
-					RECEIVE_STATE <= WAITING_FOR_ACKNOWLEDGE;
-				when WAITING_FOR_ACKNOWLEDGE =>
-					timeout_waiting_for_acknowledge_counter := timeout_waiting_for_acknowledge_counter - 1;
-					if (acknowledge_start_event_transfer = '1') then
-						internal_start_event_transfer <= '0';
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					elsif (timeout_waiting_for_acknowledge_counter = 0) then
-						internal_start_event_transfer <= '0';
-						internal_MISSING_ACKNOWLEDGEMENT_COUNTER <= std_logic_vector(unsigned(internal_MISSING_ACKNOWLEDGEMENT_COUNTER) + 1);
-						RECEIVE_STATE <= WAITING_FOR_HEADER;
-					end if;
-				when others =>
-					internal_UNKNOWN_ERROR_COUNTER <= std_logic_vector(unsigned(internal_UNKNOWN_ERROR_COUNTER) + 1);
-					RECEIVE_STATE <= WAITING_FOR_HEADER;
-			end case;
+							internal_WRONG_SCROD_ADDRESSED_COUNTER <= std_logic_vector(unsigned(internal_WRONG_SCROD_ADDRESSED_COUNTER) + 1);
+							PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+						end if;
+						PACKET_RECEIVER_STATE <= READING_CHECKSUM;
+					when READING_CHECKSUM =>
+						-- this state does not change the running checksum "checksum" like all other states do
+						checksum_from_packet := unsigned(internal_RX_D);
+						remaining_words_in_packet := remaining_words_in_packet - 1; -- should be one after this
+						PACKET_RECEIVER_STATE <= READING_FOOTER;
+					when READING_FOOTER =>
+						footer := unsigned(internal_RX_D);
+						checksum := checksum + footer;
+						remaining_words_in_packet := remaining_words_in_packet - 1; -- should be zero after this
+--						if (remaining_words_in_packet = '0') then
+--								internal_WRONG__COUNTER <= std_logic_vector(unsigned(internal_WRONG__COUNTER) + 1);
+--						end if;
+						if (footer = x"62504944") then
+							if (checksum = checksum_from_packet) then
+								COMMAND_PROCESSING_STATE <= SET_SEND_EVENT_FLAG;
+								PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+							else
+								internal_WRONG_CHECKSUM_COUNTER <= std_logic_vector(unsigned(internal_WRONG_CHECKSUM_COUNTER) + 1);
+								PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+							end if;
+						else
+							internal_WRONG_FOOTER_COUNTER <= std_logic_vector(unsigned(internal_WRONG_FOOTER_COUNTER) + 1);
+							PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+						end if;
+					when others =>
+						internal_UNKNOWN_ERROR_COUNTER <= std_logic_vector(unsigned(internal_UNKNOWN_ERROR_COUNTER) + 1);
+						PACKET_RECEIVER_STATE <= WAITING_FOR_HEADER;
+				end case;
+			else
+				case COMMAND_PROCESSING_STATE is
+					when WAITING_TO_PROCESS_COMMAND =>
+					when SET_SEND_EVENT_FLAG =>
+						internal_number_of_sent_events <= std_logic_vector(unsigned(internal_number_of_sent_events) + 1);
+						internal_start_event_transfer <= '1';
+						timeout_waiting_for_acknowledge_counter := NUMBER_OF_CYCLES_TO_WAIT_FOR_ACKNOWLEDGE;
+						COMMAND_PROCESSING_STATE <= WAITING_FOR_ACKNOWLEDGE;
+					when WAITING_FOR_ACKNOWLEDGE =>
+						timeout_waiting_for_acknowledge_counter := timeout_waiting_for_acknowledge_counter - 1;
+						if (acknowledge_start_event_transfer = '1') then
+							internal_start_event_transfer <= '0';
+							COMMAND_PROCESSING_STATE <= WAITING_TO_PROCESS_COMMAND;
+						elsif (timeout_waiting_for_acknowledge_counter = 0) then
+							internal_start_event_transfer <= '0';
+							internal_MISSING_ACKNOWLEDGEMENT_COUNTER <= std_logic_vector(unsigned(internal_MISSING_ACKNOWLEDGEMENT_COUNTER) + 1);
+							COMMAND_PROCESSING_STATE <= WAITING_TO_PROCESS_COMMAND;
+						end if;
+					when others =>
+--						internal_UNKNOWN_ERROR_COUNTER <= std_logic_vector(unsigned(internal_UNKNOWN_ERROR_COUNTER) + 1);
+						COMMAND_PROCESSING_STATE <= WAITING_TO_PROCESS_COMMAND;
+				end case;
+			end if;
 		end if;
 	end process;
 end Behavioral;
