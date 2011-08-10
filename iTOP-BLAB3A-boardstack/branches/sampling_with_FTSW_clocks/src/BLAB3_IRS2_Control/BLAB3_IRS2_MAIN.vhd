82e339d10d9dbeacf3807f8d105b1b3758f3bbfa
kurtisn@gmail.com <kurtisn@gmail.com@88b02a49-462a-2295-53a7-3445142cab38>
2011-08-10 08:04:34 +0000

    Now receives clocks from Nakao-san's FTSW blocks and runs off of them.  Sampling is tuned for 2.7 gigasamples/sec in both the chipscope file and in the code (SSP, SST, WR_STRB frequencies, etc.).  In addition, changed some of the DAC code in an attempt to remove some bugs.  The update static outputs button in chipscope now appears to work, but there is occasionally a problem where updating one DAC value will cause a different one to change.  Please be careful and watchful of this when running chipscope to modify DAC values.

--- iTOP-BLAB3A-boardstack/branches/sampling_with_FTSW_clocks/src/BLAB3_IRS2_Control/BLAB3_IRS2_MAIN.vhd
+++ iTOP-BLAB3A-boardstack/branches/sampling_with_FTSW_clocks/src/BLAB3_IRS2_Control/BLAB3_IRS2_MAIN.vhd
@@ -1,15 +1,14 @@
 --------------------------------------------------------	
 -- Design by: Kurtis Nishimura
--- Last updated: 2011-06-12
--- Notes: This firmware is to operate IRS2 or BLAB3 in 
---        "one-shot" mode, where the sample signal is 
---        given only when a software trigger is received.
---        It is primarily for simple evaluation.
+-- Last updated: 2011-08-10
+-- Notes: Stripping down firmware to work with FTSW
 --------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity BLAB3_IRS2_MAIN is
     port (
@@ -32,30 +31,36 @@ entity BLAB3_IRS2_MAIN is
 		SOFT_WRITE_ADDR   : in  std_logic_vector(8 downto 0);
 		SOFT_READ_ADDR    : in  std_logic_vector(8 downto 0);		
 		-- User I/O
-		CLK_SSP          : in  std_logic;--Sampling rate / 128 (0 deg)
-		CLK_SST          : in  std_logic;--Sampling rate / 128 (90 deg)
-		CLK_WRITE_STROBE : in  std_logic;--Sampling rate / 64  (270 deg)
+		CLK_SSP          : in  std_logic;--Sampling rate / 128
+		CLK_SST          : in  std_logic;--Sampling rate / 128
+		CLK_WRITE_STROBE : in  std_logic;--Sampling rate / 64
 
 		START_USB_XFER	   : out std_logic;--Signal to start sending data to USB
 		DONE_USB_XFER 	   : in  std_logic;
-		MON_HDR		 	   : out std_logic_vector(15 downto 0); 
+		MONITOR		 	   : out std_logic_vector(15 downto 0); 
 		CLR_ALL		 	   : in  std_logic;
 		TRIGGER			   : in  std_logic;
 		RAM_READ_ADDRESS  : in std_logic_vector(11 downto 0);
 		DATA_TO_USB       : out std_logic_vector(15 downto 0));
 end BLAB3_IRS2_MAIN;
 
-architecture Behavioral of BLAB3_IRS2_MAIN is
+architecture implementation of BLAB3_IRS2_MAIN is
    attribute BOX_TYPE   : string ;
---------------------------------------------------------------------------------
---   								signals		     		   						         --
---------------------------------------------------------------------------------
+	--------------------------------------------------------------------------------
+	--   								signals		     		   						         --
+	--------------------------------------------------------------------------------
+	signal internal_ASIC_WR_STRB	: std_logic;
+	signal internal_ASIC_SSP_IN	: std_logic;
+	signal internal_ASIC_SST_IN	: std_logic;
+	signal internal_ASIC_WR_ADDR	: std_logic_vector(9 downto 0);	
+	signal internal_MONITOR			: std_logic_vector(15 downto 0);
+
 	type STATE_TYPE is ( WAITING, NOMINAL_SAMPLING,
 								ARM_WILKINSON,PERFORM_WILKINSON,
 								ARM_READING,READ_TO_RAM,WAIT_FOR_READ_SETTLING,
-								READOUT_BY_USB);
-	
+								READOUT_BY_USB);	
 	signal internal_STATE          : STATE_TYPE;
+
 
 	signal internal_RAM_OUTPUT_DATA        : std_logic_vector(15 downto 0);
 	signal internal_RAM_READ_ENABLE        : std_logic;
@@ -73,10 +78,6 @@ architecture Behavioral of BLAB3_IRS2_MAIN is
 	signal internal_ASIC_DAT		    : std_logic_vector(11 downto 0);
 	signal internal_ASIC_TDC_START    : std_logic; 
 	signal internal_ASIC_TDC_CLR	    : std_logic; 
-	signal internal_ASIC_WR_STRB	    : std_logic; 
-	signal internal_ASIC_WR_ADDR	    : std_logic_vector(9 downto 0) := (others => '0');
-	signal internal_ASIC_SSP_IN	    : std_logic;
-	signal internal_ASIC_SST_IN	    : std_logic;		
 	signal internal_ASIC_SSP_OUT	    : std_logic;
 	signal internal_ASIC_TRIGGER_BITS : std_logic_vector(7 downto 0);
 	signal internal_SOFT_WRITE_ADDR   : std_logic_vector(8 downto 0);
@@ -86,89 +87,220 @@ architecture Behavioral of BLAB3_IRS2_MAIN is
 	signal internal_BUSY              : std_logic;
 
 	signal internal_CLK_STATE_MACHINE_DIV_BY_2 : std_logic;
-
 -------------------------------------------------------------------------
-	component MULTI_WINDOW_RAM_BLOCK
-	port (
-		clka: IN std_logic;
-		ena: IN std_logic;
-		wea: IN std_logic_VECTOR(0 downto 0);
-		addra: IN std_logic_VECTOR(11 downto 0);
-		dina: IN std_logic_VECTOR(15 downto 0);
-		clkb: IN std_logic;
-		addrb: IN std_logic_VECTOR(11 downto 0);
-		doutb: OUT std_logic_VECTOR(15 downto 0));
-	end component;
 -------------------------------------------------------------------------
 begin
 -------------------------------------------------------------------------			
-	READOUT_RAM_BLOCK : MULTI_WINDOW_RAM_BLOCK
-	port map (
-		clka => CLK_WRITE_STROBE,
-		ena => '1',
-		wea => internal_RAM_WRITE_ENABLE_VEC,
-		addra => internal_RAM_WRITE_ADDRESS,
-		dina => internal_RAM_INPUT_DATA,
-		clkb => not(CLK_WRITE_STROBE),
-		addrb => RAM_READ_ADDRESS,
-		doutb => DATA_TO_USB);		
--------------------------------------------------------------------------
-internal_RAM_INPUT_DATA(15 downto 12) <= (others => '0');
-internal_RAM_INPUT_DATA(11 downto 0) <= internal_ASIC_DAT;
-MON_HDR(0) <= internal_ASIC_SSP_IN;
-MON_HDR(1) <= internal_ASIC_SST_IN;
-MON_HDR(2) <= internal_ASIC_WR_STRB;
-MON_HDR(3) <= internal_ASIC_WR_ADDR(0);
-MON_HDR(4) <= internal_ASIC_SSP_OUT;
---MON_HDR(12 downto 5) <= internal_ASIC_TRIGGER_BITS;
-MON_HDR(5) <= internal_ASIC_TDC_CLR;
-MON_HDR(6) <= internal_ASIC_TDC_START;
-MON_HDR(7) <= internal_ASIC_RAMP;
-MON_HDR(13 downto 8) <= (others => '0');
-MON_HDR(14) <= internal_ASIC_WR_ADDR(9);
-MON_HDR(15) <= internal_CLK_STATE_MACHINE_DIV_BY_2;
 
-ASIC_CH_SEL   <= internal_ASIC_CH_SEL;
-ASIC_RD_ADDR  <= internal_ASIC_RD_ADDR;
-ASIC_SMPL_SEL <= internal_ASIC_SMPL_SEL;
-ASIC_SMPL_SEL_ALL <= internal_ASIC_SMPL_SEL_ALL; 
-ASIC_RD_ENA <= internal_ASIC_RD_ENA; 
-ASIC_RAMP <= internal_ASIC_RAMP; 
-ASIC_TDC_START <= internal_ASIC_TDC_START; 
-ASIC_TDC_CLR <= internal_ASIC_TDC_CLR; 
-ASIC_WR_ADDR <= internal_ASIC_WR_ADDR;
-ASIC_WR_STRB <= internal_ASIC_WR_STRB;
-ASIC_SSP_IN <= internal_ASIC_SSP_IN;
-ASIC_SST_IN <= internal_ASIC_SST_IN;
+	-------WIRING BETWEEN INTERNALS AND IOs-------------
+	--The following are covered by ODDR2 blocks since they're
+	--  based on clock signals.
+	--ASIC_WR_STRB 	<= internal_ASIC_WR_STRB;
+	--ASIC_SSP_IN		<= internal_ASIC_SSP_IN;
+	--ASIC_SST_IN		<= internal_ASIC_SST_IN;
+	--ASIC_WR_ADDR(0)	<= internal_ASIC_WR_ADDR(0);
 
-internal_ASIC_DAT <= ASIC_DAT;
-internal_ASIC_SSP_OUT <= ASIC_SSP_OUT;
-internal_SOFT_WRITE_ADDR <= SOFT_WRITE_ADDR;
-internal_SOFT_READ_ADDR <= SOFT_READ_ADDR;		
-internal_ASIC_TRIGGER_BITS <= ASIC_TRIGGER_BITS;
+	--The rest go here
+	ASIC_CH_SEL   <= internal_ASIC_CH_SEL;
+	ASIC_RD_ADDR  <= internal_ASIC_RD_ADDR;
+	ASIC_SMPL_SEL <= internal_ASIC_SMPL_SEL;
+	ASIC_SMPL_SEL_ALL <= internal_ASIC_SMPL_SEL_ALL; 
+	ASIC_RD_ENA <= internal_ASIC_RD_ENA; 
+	ASIC_RAMP <= internal_ASIC_RAMP; 
+	ASIC_TDC_START <= internal_ASIC_TDC_START; 
+	ASIC_TDC_CLR <= internal_ASIC_TDC_CLR; 
+	ASIC_WR_ADDR(9 downto 1) <= internal_ASIC_WR_ADDR(9 downto 1);
+	internal_ASIC_DAT <= ASIC_DAT;
+	internal_ASIC_SSP_OUT <= ASIC_SSP_OUT;
+	internal_SOFT_WRITE_ADDR <= SOFT_WRITE_ADDR;
+	internal_SOFT_READ_ADDR <= SOFT_READ_ADDR;		
+	internal_ASIC_TRIGGER_BITS <= ASIC_TRIGGER_BITS;
 
-internal_TRIGGER <= TRIGGER;
+	internal_TRIGGER <= TRIGGER;
 
-internal_RAM_WRITE_ENABLE_VEC(0) <= internal_RAM_WRITE_ENABLE;
+	internal_RAM_WRITE_ENABLE_VEC(0) <= internal_RAM_WRITE_ENABLE;
+	----------------------------------------------------
 
--------LOGIC TO RUN ASIC SAMPLING------
-internal_ASIC_WR_STRB <= CLK_WRITE_STROBE;
-internal_ASIC_SSP_IN <= CLK_SSP;
-internal_ASIC_SST_IN <= CLK_SST;
-internal_ASIC_WR_ADDR(0) <= CLK_SST;
----------------------------------------	
+	-------LOGIC TO RUN ASIC SAMPLING------
+	internal_ASIC_WR_STRB <= CLK_WRITE_STROBE;
+	internal_ASIC_SSP_IN <= CLK_SSP;
+	internal_ASIC_SST_IN <= CLK_SST;
+	internal_ASIC_WR_ADDR(0) <= CLK_SST;
+
+	-------MONITOR HEADER------------------
+	internal_MONITOR(0) <= internal_ASIC_SSP_IN;
+	internal_MONITOR(1) <= internal_ASIC_SST_IN;
+	internal_MONITOR(2) <= internal_ASIC_WR_STRB;
+	internal_MONITOR(3) <= internal_ASIC_WR_ADDR(0);
+	internal_MONITOR(4) <= internal_ASIC_SSP_OUT;
+	internal_MONITOR(15) <= internal_ASIC_WR_ADDR(9);
+	internal_MONITOR(14 downto 5) <= (others => '0');
+
+	--Signals 15 downto 4 are simple pass throughs.
+	--Signals 3 downto 0 come from clocks, 
+	--  so must be driven by ODDR2 primitives.
+	MONITOR(15 downto 4) <= internal_MONITOR(15 downto 4);
+	---------------------------------------	
+
+	-------------------------------------------------------------------------			
+	READOUT_RAM_BLOCK : entity work.MULTI_WINDOW_RAM_BLOCK
+		port map (
+			clka => CLK_WRITE_STROBE,
+			ena => '1',
+			wea => internal_RAM_WRITE_ENABLE_VEC,
+			addra => internal_RAM_WRITE_ADDRESS,
+			dina => internal_RAM_INPUT_DATA,
+			clkb => not(CLK_WRITE_STROBE),
+			addrb => RAM_READ_ADDRESS,
+			doutb => DATA_TO_USB);		
+			
+	internal_RAM_INPUT_DATA(15 downto 12) <= (others => '0');
+	internal_RAM_INPUT_DATA(11 downto 0) <= internal_ASIC_DAT;
+	-------------------------------------------------------------------------
+	---------------------------------------------------------
+	ODDR2_MON0 : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => MONITOR(0), -- 1-bit output data
+			C0 => internal_MONITOR(0), -- 1-bit clock input
+			C1 => not(internal_MONITOR(0)), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	---------------------------------------------------------
+	ODDR2_MON1 : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => MONITOR(1), -- 1-bit output data
+			C0 => internal_MONITOR(1), -- 1-bit clock input
+			C1 => not(internal_MONITOR(1)), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	---------------------------------------------------------
+	ODDR2_MON2 : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => MONITOR(2), -- 1-bit output data
+			C0 => internal_MONITOR(2), -- 1-bit clock input
+			C1 => not(internal_MONITOR(2)), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	---------------------------------------------------------	
+	ODDR2_MON3 : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => MONITOR(3), -- 1-bit output data
+			C0 => internal_MONITOR(3), -- 1-bit clock input
+			C1 => not(internal_MONITOR(3)), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	---------------------------------------------------------		
+
+	---------------------------------------------------------		
+	ODDR2_SSP_IN : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => ASIC_SSP_IN, -- 1-bit output data
+			C0 => internal_ASIC_SSP_IN, -- 1-bit clock input
+			C1 => not(internal_ASIC_SSP_IN), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	---------------------------------------------------------
+	ODDR2_SST_IN : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => ASIC_SST_IN, -- 1-bit output data
+			C0 => internal_ASIC_SST_IN, -- 1-bit clock input
+			C1 => not(internal_ASIC_SST_IN), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	---------------------------------------------------------
+	ODDR2_WR_STRB : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => ASIC_WR_STRB, -- 1-bit output data
+			C0 => internal_ASIC_WR_STRB, -- 1-bit clock input
+			C1 => not(internal_ASIC_WR_STRB), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	---------------------------------------------------------
+	ODDR2_WR_ADDR0 : ODDR2
+		generic map(
+			DDR_ALIGNMENT => "NONE", -- Sets output alignment to "NONE", "C0", "C1"
+			INIT => '0', -- Sets initial state of the Q output to '0' or '1'
+			SRTYPE => "SYNC") -- Specifies "SYNC" or "ASYNC" set/reset
+		port map (
+			Q => ASIC_WR_ADDR(0), -- 1-bit output data
+			C0 => internal_ASIC_WR_ADDR(0), -- 1-bit clock input
+			C1 => not(internal_ASIC_WR_ADDR(0)), -- 1-bit clock input
+			CE => '1', -- 1-bit clock enable input
+			D0 => '1', -- 1-bit data input (associated with C0)
+			D1 => '0', -- 1-bit data input (associated with C1)
+			R => '0', -- 1-bit reset input
+			S => '0' -- 1-bit set input
+	);
+	-----------------------------------
 
 --------------------------------------------------------------------------------
 process(CLK_SST, internal_STATE, CLR_ALL, DONE_USB_XFER, internal_BUSY)
 	variable delay_counter : integer range 0 to 1023 := 0;
 	constant time_to_arm_wilkinson : integer := 3; -- A guess... should just buy some extra time for logic to settle
-	constant time_to_wilkinson : integer := 97; -- 6.2 us @ 15.625 MHz
+--	constant time_to_wilkinson : integer := 97; -- 6.2 us @ 15.625 MHz
+	constant time_to_wilkinson : integer := 128; -- 6.2 us @ 21.2 MHz	
 	constant read_to_ram_settling_time : integer := 1; --In principle we should only need 1 clock cycle here.
 	variable windows_sampled_after_trigger : integer range 0 to 1023 := 0;
 	variable windows_read_out : integer range 0 to 1023 := 0;
 	variable samples_read_out_this_window : integer range 0 to 1023;
 	constant windows_to_sample : integer := 8;
-	constant trigger_arming_time : integer := 1;
+	constant trigger_arming_time : integer := 2;
 begin
 ------------Asynchronous reset state------------------------
 	if (CLR_ALL = '1' or DONE_USB_XFER = '1') then
@@ -195,7 +327,7 @@ begin
 				if (internal_TRIGGER <= '1') then
 				   internal_BUSY <= '1';
 					internal_ASIC_WR_ADDR(9) <= '1';
-					if (delay_counter < trigger_arming_time) then
+					if (delay_counter >= trigger_arming_time) then
 						internal_STATE <= NOMINAL_SAMPLING;
 						windows_sampled_after_trigger := 2;
 						delay_counter := 0;
@@ -297,4 +429,4 @@ begin
 	end if;
 end process;
 --------------------------------------------------------------------------------
-end Behavioral;
+end implementation;
