e8fd5758580c61cfb4afb7c2df06e8807d2bb025
kurtisn@gmail.com <kurtisn@gmail.com@88b02a49-462a-2295-53a7-3445142cab38>
2011-09-12 04:49:00 +0000

    Added DAC functionality.  Check the board_stack_definitions file for the conventions for DAC addressing.  Interface right now is Chipscope only, so you can only target one channel at a time.

--- iTOP-BLAB3A-boardstack/branches/beam_test_or_bust/src/Board_Stack_Definitions.vhd
+++ iTOP-BLAB3A-boardstack/branches/beam_test_or_bust/src/Board_Stack_Definitions.vhd
@@ -10,8 +10,30 @@ use IEEE.STD_LOGIC_1164.all;
 package Board_Stack_Definitions is
 	-- A single LTC2637 has 8 DAC channels, each 12 bits
 	type LTC2637_Voltages is array(7 downto 0) of std_logic_vector(11 downto 0);
-	-- There are 2 LTC2637 DACs per daughter card.
-   type Daughter_Card_Voltages is array(1 downto 0) of LTC2637_Voltages;
+	-- There are 2 LTC2637 DACs per daughter card, so 8 per column.
+   type Column_Voltages is array(7 downto 0) of LTC2637_Voltages;
+	type Board_Stack_Voltages is array(3 downto 0) of Column_Voltages;
+
+	-- Address conventions for the board stack DACs
+	subtype LTC2637_Address is std_logic_vector(6 downto 0);
+	-- The following are the chosen address conventions
+	-- Please note that the "row" refers to the carrier level
+	-- and the index 0/1 correspond to which DAC on the daughter
+	-- card.  
+	-- Index 0 : DAC1
+	-- Index 1 : DAC2  
+	-- *Recommend a name change on the schematic for consistency.
+	-- 
+	-- Name (R<row>_(0/1))								IIC Address		CA(2)	CA(1)	CA(0)
+	constant DAC_Address_R0_0	: LTC2637_Address := "0010000";--GND	GND	GND
+	constant DAC_Address_R0_1	: LTC2637_Address := "0010001";--GND	GND	FLT
+	constant DAC_Address_R1_0	: LTC2637_Address := "0010011";--GND	FLT	GND
+	constant DAC_Address_R1_1	: LTC2637_Address := "0100000";--GND	FLT	FLT
+	constant DAC_Address_R2_0	: LTC2637_Address := "0110001";--FLT	GND	GND
+	constant DAC_Address_R2_1	: LTC2637_Address := "0110010";--FLT	GND	FLT
+	constant DAC_Address_R3_0	: LTC2637_Address := "1000000";--FLT	FLT	GND
+	constant DAC_Address_R3_1	: LTC2637_Address := "1000001";--FLT	FLT	FLT 
+   constant DAC_Write_and_Update : std_logic_vector(3 downto 0) := "0011";
   
 end Board_Stack_Definitions;
 
--- iTOP-BLAB3A-boardstack/branches/beam_test_or_bust/src/iTOP_Board_Stack_DAC_Control.vhd
+++ iTOP-BLAB3A-boardstack/branches/beam_test_or_bust/src/iTOP_Board_Stack_DAC_Control.vhd
@@ -31,47 +31,36 @@ use IEEE.NUMERIC_STD.ALL;
 
 use work.Board_Stack_Definitions.ALL;
 
-entity iTOP_Board_Stack_DAC_Control is
-    Port ( INTENDED_DAC_VALUES	: in Daughter_Card_Voltages;
-			  CURRENT_DAC_VALUES 	: out Daughter_Card_Voltages;
-			  IIC_CLK        : in  std_logic;
-           SCL_DC1 		  : out std_logic;
-			  SDA_DC1		  : inout std_logic;
-			  STATE_MONITOR  : out std_logic_vector(2 downto 0)
+
+-------------------------
+-------------------------
+-------------------------
+
+entity Board_Stack_Column_DAC_Control is
+    Port ( INTENDED_DAC_VALUES	: in Column_Voltages;
+			  CURRENT_DAC_VALUES 	: out	Column_Voltages;
+			  IIC_CLK        			: in  std_logic;
+           SCL 						: out std_logic;
+			  SDA		  					: inout std_logic;
+			  STATE_MONITOR			: out std_logic_vector(2 downto 0)
 			  );
-end iTOP_Board_Stack_DAC_Control;
+end Board_Stack_Column_DAC_Control;
 
-architecture Behavioral of iTOP_Board_Stack_DAC_Control is
+architecture Behavioral of Board_Stack_Column_DAC_Control is
 ---------------------------------------------------------------
-	component LTC2637_I2C_Interface is
-		 Port ( SCL				: out STD_LOGIC;
-				  SDA				: inout STD_LOGIC;
-				  IIC_CLK      : in STD_LOGIC;
-				  ADDRESS      : in STD_LOGIC_VECTOR(6 downto 0);
-				  COMMAND      : in STD_LOGIC_VECTOR(3 downto 0);
-				  CHANNEL      : in STD_LOGIC_VECTOR(3 downto 0);
-				  DAC_VALUE    : in STD_LOGIC_VECTOR(11 downto 0);
-				  UPDATE       : in STD_LOGIC;				  
-				  UPDATING     : out STD_LOGIC;
-				  UPDATE_SUCCEEDED    : out STD_LOGIC);
-	end component;
-   ----------------------------------------------------------
-	signal internal_INTENDED_DAC_VALUES_DC1 : Daughter_Card_Voltages;	
-	signal internal_CURRENT_DAC_VALUES_DC1  : Daughter_Card_Voltages;
-	signal internal_SCL_DC1                 : std_logic;
-	signal internal_SDA_DC1                 : std_logic;
-	signal internal_ADDRESS_DC1		       : std_logic_vector(6 downto 0);
-	signal internal_COMMAND_DC1             : std_logic_vector(3 downto 0);
-	signal internal_CHANNEL_DC1             : std_logic_vector(3 downto 0);
-	signal internal_DAC_VALUE_DC1           : std_logic_vector(11 downto 0);
-	signal internal_UPDATE_DC1              : std_logic;
-	signal internal_UPDATING_DC1 				 : std_logic;
-	signal internal_UPDATE_SUCCEEDED_DC1    : std_logic;
-	
-	signal internal_ADDRESS1_DC1            : std_logic_vector(6 downto 0);
-	signal internal_ADDRESS2_DC1            : std_logic_vector(6 downto 0);
+	signal internal_INTENDED_DAC_VALUES : Column_Voltages;	
+	signal internal_CURRENT_DAC_VALUES  : Column_Voltages;
+	signal internal_SCL                 : std_logic;
+	signal internal_SDA                 : std_logic;
+	signal internal_ADDRESS		       	: std_logic_vector(6 downto 0);
+	signal internal_COMMAND             : std_logic_vector(3 downto 0);
+	signal internal_CHANNEL             : std_logic_vector(3 downto 0);
+	signal internal_DAC_VALUE           : std_logic_vector(11 downto 0);
+	signal internal_UPDATE              : std_logic;
+	signal internal_UPDATING 				: std_logic;
+	signal internal_UPDATE_SUCCEEDED    : std_logic;
 
-	signal internal_DAC_STATE_MONITOR       : std_logic_vector(2 downto 0);
+	signal internal_DAC_STATE_MONITOR	: std_logic_vector(2 downto 0);
 
 	type DAC_STATE_TYPE is (SEARCHING_FOR_UPDATES, SETTING_UP_UPDATES, START_UPDATING, WAIT_FOR_UPDATE);
 	signal DAC_STATE      : DAC_STATE_TYPE := SEARCHING_FOR_UPDATES;
@@ -79,49 +68,47 @@ architecture Behavioral of iTOP_Board_Stack_DAC_Control is
 --------------------------------------------------------------	
 begin
    ----------------------------------------------------------
-	DC1_LTC2637 : LTC2637_I2C_Interface
+	COL_LTC2637 : entity work.LTC2637_I2C_Interface
 	port map (
-		SCL => internal_SCL_DC1,
-		SDA => internal_SDA_DC1,
+		SCL => internal_SCL,
+		SDA => internal_SDA,
 		IIC_CLK => IIC_CLK,
-		UPDATE => internal_UPDATE_DC1,
-		ADDRESS => internal_ADDRESS_DC1,
-		COMMAND => internal_COMMAND_DC1,
-		CHANNEL => internal_CHANNEL_DC1,
-		DAC_VALUE => internal_DAC_VALUE_DC1,
-		UPDATING => internal_UPDATING_DC1,
-		UPDATE_SUCCEEDED => internal_UPDATE_SUCCEEDED_DC1
+		UPDATE => internal_UPDATE,
+		ADDRESS => internal_ADDRESS,
+		COMMAND => internal_COMMAND,
+		CHANNEL => internal_CHANNEL,
+		DAC_VALUE => internal_DAC_VALUE,
+		UPDATING => internal_UPDATING,
+		UPDATE_SUCCEEDED => internal_UPDATE_SUCCEEDED
 	);
    -----------------------------------------------------------
-	SCL_DC1 <= internal_SCL_DC1;
-	SDA_DC1 <= internal_SDA_DC1;
-	internal_INTENDED_DAC_VALUES_DC1 <= INTENDED_DAC_VALUES;
-	CURRENT_DAC_VALUES <= internal_CURRENT_DAC_VALUES_DC1;
-	internal_ADDRESS1_DC1 <= "1000000"; -- For BLAB3A DC card 2	
-	internal_ADDRESS2_DC1 <= "1000001"; -- For BLAB3A DC card 2
---	internal_ADDRESS1_DC1 <= "0010000"; -- For BLAB3A DC card 1	
---	internal_ADDRESS2_DC1 <= "0110010"; -- For BLAB3A DC card 1
-	internal_COMMAND_DC1  <= "0010";
+	SCL <= internal_SCL;
+	SDA <= internal_SDA;
+	internal_INTENDED_DAC_VALUES <= INTENDED_DAC_VALUES;
+	CURRENT_DAC_VALUES <= internal_CURRENT_DAC_VALUES;
+	internal_COMMAND <= DAC_Write_and_Update;
 	
 	STATE_MONITOR <= internal_DAC_STATE_MONITOR;
 --------------------------------------------------------------
 	
 	process (IIC_CLK) 
 		variable dac_channel : integer range 0 to 7 := 0;
-		variable dac_address : integer range 0 to 1 := 0;
+		variable dac_address : integer range 0 to 7 := 0;
+		constant dac_channels : integer := 8;
+		constant dac_addresses: integer := 8;
 	begin
 		if (falling_edge(IIC_CLK)) then
 			case DAC_STATE is
 				when SEARCHING_FOR_UPDATES =>
 					internal_DAC_STATE_MONITOR	<= "000";
-					if (internal_INTENDED_DAC_VALUES_DC1(dac_address)(dac_channel) /= internal_CURRENT_DAC_VALUES_DC1(dac_address)(dac_channel)) then
+					if (internal_INTENDED_DAC_VALUES(dac_address)(dac_channel) /= internal_CURRENT_DAC_VALUES(dac_address)(dac_channel)) then
 						DAC_STATE <= SETTING_UP_UPDATES;
 					else 
-						if (dac_channel < 7) then
+						if (dac_channel < dac_channels - 1) then
 							dac_channel := dac_channel + 1;
 						else
 							dac_channel := 0;
-							if (dac_address < 1) then
+							if (dac_address < dac_addresses - 1) then
 								dac_address := dac_address + 1;
 							else
 								dac_address := 0;
@@ -130,32 +117,37 @@ begin
 					end if;
 				when SETTING_UP_UPDATES =>
 					internal_DAC_STATE_MONITOR	<= "001";
-					if (dac_address = 0) then
-						internal_ADDRESS_DC1 <= internal_ADDRESS1_DC1;
-					elsif (dac_address = 1) then
-						internal_ADDRESS_DC1 <= internal_ADDRESS2_DC1;						
-					end if;
-					internal_CHANNEL_DC1 <= std_logic_vector(to_unsigned(dac_channel, 4));
-					internal_DAC_VALUE_DC1 <= internal_INTENDED_DAC_VALUES_DC1(dac_address)(dac_channel);
+					case dac_address is
+						when 0 => internal_ADDRESS <= DAC_Address_R0_0;
+						when 1 => internal_ADDRESS <= DAC_Address_R0_1;
+						when 2 => internal_ADDRESS <= DAC_Address_R1_0;
+						when 3 => internal_ADDRESS <= DAC_Address_R1_1;
+						when 4 => internal_ADDRESS <= DAC_Address_R2_0;
+						when 5 => internal_ADDRESS <= DAC_Address_R2_1;
+						when 6 => internal_ADDRESS <= DAC_Address_R3_0;
+						when 7 => internal_ADDRESS <= DAC_Address_R3_1;	
+					end case;
+					internal_CHANNEL <= std_logic_vector(to_unsigned(dac_channel, 4));
+					internal_DAC_VALUE <= internal_INTENDED_DAC_VALUES(dac_address)(dac_channel);
 					DAC_STATE <= START_UPDATING;
 				when START_UPDATING =>
 					internal_DAC_STATE_MONITOR	<= "010";				
-					internal_UPDATE_DC1 <= '1';
-					if (internal_UPDATING_DC1 = '1') then
-						internal_UPDATE_DC1 <= '0';
+					internal_UPDATE <= '1';
+					if (internal_UPDATING = '1') then
+						internal_UPDATE <= '0';
 						DAC_STATE <= WAIT_FOR_UPDATE;
 					end if;
 				when WAIT_FOR_UPDATE =>
 					internal_DAC_STATE_MONITOR	<= "011";
-					if (internal_UPDATING_DC1 = '0') then
-						if (internal_UPDATE_SUCCEEDED_DC1 = '1') then
-							internal_CURRENT_DAC_VALUES_DC1(dac_address)(dac_channel) <= INTENDED_DAC_VALUES(dac_address)(dac_channel);
+					if (internal_UPDATING = '0') then
+						if (internal_UPDATE_SUCCEEDED = '1') then
+							internal_CURRENT_DAC_VALUES(dac_address)(dac_channel) <= INTENDED_DAC_VALUES(dac_address)(dac_channel);
 						end if;
-						if (dac_channel < 7) then
+						if (dac_channel < dac_channels - 1) then
 							dac_channel := dac_channel + 1;
 						else
 							dac_channel := 0;
-							if (dac_address < 1) then
+							if (dac_address < dac_addresses - 1) then
 								dac_address := dac_address + 1;
 							else
 								dac_address := 0;
@@ -170,4 +162,41 @@ begin
 	end process;
 	
 end Behavioral;
+
+-------------------------
+-------------------------
+-------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+use work.Board_Stack_Definitions.ALL;
+
+entity iTOP_Board_Stack_DAC_Control is
+	Port ( 
+		INTENDED_DAC_VALUES	: in Board_Stack_Voltages;
+		CURRENT_DAC_VALUES 	: out Board_Stack_Voltages;
+		CLK_100kHz_MAX      	: in  std_logic;
+      SCL_C 		  			: out std_logic_vector(3 downto 0);
+		SDA_C		  				: inout std_logic_vector(3 downto 0)
+	);
+end iTOP_Board_Stack_DAC_Control;
+
+architecture Behavioral of iTOP_Board_Stack_DAC_Control is
+
+begin
+	DAC_Control_COLGEN : for i in 0 to 3 generate
+		DAC_Control_COL : entity work.Board_Stack_Column_DAC_Control
+			port map (
+				INTENDED_DAC_VALUES	=> INTENDED_DAC_VALUES(i),
+				CURRENT_DAC_VALUES	=> CURRENT_DAC_VALUES(i),
+				IIC_CLK					=> CLK_100kHz_MAX,
+				SCL						=> SCL_C(i),
+				SDA						=> SDA_C(i),
+				STATE_MONITOR			=> open
+			);
+	end generate;
+end Behavioral;
+
 
