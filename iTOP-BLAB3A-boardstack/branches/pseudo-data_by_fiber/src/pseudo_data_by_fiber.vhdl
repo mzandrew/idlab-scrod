349dfa9eca7cbcf370bfd5b4b50d4fc4ac526977
mzandrew@gmail.com <mzandrew@gmail.com@88b02a49-462a-2295-53a7-3445142cab38>
2011-09-28 10:22:56 +0000

    modularized trigger

--- iTOP-BLAB3A-boardstack/branches/pseudo-data_by_fiber/src/pseudo_data_by_fiber.vhdl
+++ iTOP-BLAB3A-boardstack/branches/pseudo-data_by_fiber/src/pseudo_data_by_fiber.vhdl
@@ -55,46 +55,36 @@ library UNISIM;
 use UNISIM.VCOMPONENTS.ALL;
 
 architecture PDBF of pseudo_data_by_fiber is
+	signal global_reset : std_logic := '1';
+	signal request_a_global_reset : std_logic := '0';
+-- clocks -------------------------------------------------------------------
+	signal internal_clock_from_remote_source : std_logic;
+	signal internal_clock_250MHz             : std_logic;
+	signal internal_COUNTER : std_logic_vector(31 downto 0);
+	signal clock_1MHz : std_logic;
+	signal clock_1kHz : std_logic;
+	signal internal_clock_for_state_machine : std_logic;
+	signal clock_select : std_logic := '0'; -- '0' = local; '1' = remote
 -- chipscope signals --------------------------------------------------------
 	signal chipscope_vio_buttons : std_logic_vector(255 downto 0);
 	signal chipscope_vio_display : std_logic_vector(255 downto 0);
 	signal chipscope_ila_data    : std_logic_vector(255 downto 0) := (others => '0');
 	signal chipscope_ila_trigger : std_logic_vector(255 downto 0) := (others => '0');
------------------------------------------------------------------------------
-	signal internal_clock_from_remote_source : std_logic;
-	signal internal_clock_250MHz             : std_logic;
-	signal internal_COUNTER : std_logic_vector(31 downto 0);
------------------------------------------------------------------------------
-	signal clock_1MHz : std_logic;
-	signal clock_1kHz : std_logic;
------------------------------------------------------------------------------
--- trigger signals:
-	signal external_trigger_1_from_monitor_header : std_logic;
-	signal external_trigger_2_from_LVDS           : std_logic;
-	signal external_encoded_trigger_from_LVDS     : std_logic;
-	signal raw_500Hz_fake_trigger : std_logic := '0';
-	signal raw_100Hz_fake_trigger : std_logic := '0';
-	signal raw_5Hz_fake_trigger   : std_logic := '0';
-	signal external_triggers_ORed_together : std_logic;
-	signal gated_trigger : std_logic;
-	signal external_trigger_disable : std_logic := '0';
-	signal internal_trigger : std_logic;
-	signal gated_fill_inactive : std_logic;
-	signal trigger_a_digitization_and_readout_event : std_logic;
-	signal pulsed_trigger : std_logic := '0';
-	signal trigger_acknowledge : std_logic;
------------------------------------------------------------------------------
+	signal fiber_readout_chipscope_vio_buttons     : std_logic_vector(255 downto 0) := (others => '0');
+	signal fiber_readout_chipscope_vio_display     : std_logic_vector(255 downto 0) := (others => '0');
+	signal fake_button : std_logic := '1';
+-- trigger signals ----------------------------------------------------------
+	signal external_trigger_1_from_monitor_header   : std_logic;
+	signal external_trigger_2_from_LVDS             : std_logic;
+	signal external_encoded_trigger_from_LVDS       : std_logic;
+	signal external_trigger_disable                 : std_logic := '0';
+	signal internal_TRIGGER                         : std_logic := '0';
+	signal fake_spill_structure_enable              : std_logic;
+	signal spill_active                             : std_logic;
+	signal raw_5Hz_fake_trigger                     : std_logic := '0';
+-- aurora/fiber signals -----------------------------------------------------
 	signal transmit_disable : std_logic := '0';
 	signal transmit_always : std_logic;
-	signal spill_active : std_logic;
-	signal fill_active  : std_logic;
-	signal fake_spill_structure_enable : std_logic;
------------------------------------------------------------------------------
-	signal internal_clock_for_state_machine : std_logic;
-	signal clock_select : std_logic := '0'; -- '0' = local; '1' = remote
-	signal global_reset : std_logic := '1';
-	signal request_a_global_reset : std_logic := '0';
------------------------------------------------------------------------------
 	signal request_a_fiber_link_reset                         : std_logic;
 	signal should_not_automatically_try_to_keep_fiber_link_up : std_logic;
 	signal fiber_link_is_up                                   : std_logic;
@@ -107,13 +97,7 @@ architecture PDBF of pseudo_data_by_fiber is
 	signal internal_INPUT_BLOCK_RAM_ADDRESS            : std_logic_vector(NUMBER_OF_INPUT_BLOCK_RAMS-1  downto 0);
 	signal internal_ASIC_DATA_BLOCKRAM_DATA_BUS        : std_logic_vector(WIDTH_OF_ASIC_DATA_BLOCKRAM_DATA_BUS-1        downto 0);
 	signal internal_ASIC_DATA_BLOCKRAM_ADDRESS_BUS     : std_logic_vector(WIDTH_OF_ASIC_DATA_BLOCKRAM_ADDRESS_BUS-1     downto 0);
------------------------------------------------------------------------------
-	signal fiber_readout_chipscope_vio_buttons     : std_logic_vector(255 downto 0) := (others => '0');
-	signal fiber_readout_chipscope_vio_display     : std_logic_vector(255 downto 0) := (others => '0');
------------------------------------------------------------------------------
-	signal fake_button : std_logic := '1';
 begin
------------------------------------------------------------------------------
 	process(internal_clock_for_state_machine, request_a_global_reset)
 		variable internal_COUNTER  : integer range 0 to 250000000 := 0;
 	begin
@@ -183,120 +167,7 @@ begin
 --			end if;
 		end if;
 	end process;
-	process(clock_1kHz, global_reset)
-		variable counter_1_kHz    : integer range 0 to 10 := 0;
-		variable counter_200_Hz   : integer range 0 to 10 := 0;
-		variable counter_100_Hz   : integer range 0 to 10 := 0;
-		variable counter_10_Hz    : integer range 0 to 10 := 0;
-		variable counter_1_Hz     : integer range 0 to 3600 := 0;
-		variable spill_counter         : integer range 0 to 60 := 1;
-		constant spill_counter_maximum : integer range 0 to 60 := 2;
-		variable fill_counter          : integer range 0 to 60 := 1;
-		constant fill_counter_maximum  : integer range 0 to 60 := 4;
-	begin
-		if (global_reset = '1') then
-			spill_active <= '1';
-			fill_active  <= '0';
-			counter_1_kHz   := 0;
-			counter_200_Hz  := 0;
-			counter_100_Hz  := 0;
-			counter_10_Hz   := 0;
-			counter_1_Hz    := 0;
-			spill_counter   := 1;
-			fill_counter    := 1;
-			raw_5Hz_fake_trigger   <= '0';
-			raw_100Hz_fake_trigger <= '0';
-			raw_500Hz_fake_trigger <= '0';
-		elsif rising_edge(clock_1kHz) then
-			-- evaluated once per millisecond
-			counter_1_kHz := counter_1_kHz + 1;
-			if (counter_1_kHz > 4) then
-				-- evaluated 200 times per second
-				counter_1_kHz := 0;
-				counter_200_Hz := counter_200_Hz + 1;
-				raw_100Hz_fake_trigger <= not raw_100Hz_fake_trigger;
-			end if;
-			if (counter_200_Hz > 1) then
-				-- evaluated 100 times per second
-				counter_200_Hz := 0;
-				counter_100_Hz := counter_100_Hz + 1;
-			end if;
-			if (counter_100_Hz > 9) then
-				-- evaluated 10 times per second
-				counter_100_Hz := 0;
-				counter_10_Hz := counter_10_Hz + 1;
-				raw_5Hz_fake_trigger <= not raw_5Hz_fake_trigger;
-			end if;
---				raw_25Hz_fake_trigger <= not raw_25Hz_fake_trigger;
-			if (counter_10_Hz > 9) then
-				-- evaluated once per second
---				if (fiber_link_is_up = '0') then
---					pulsed_fiber_link_is_down = '1';
---					request_an_aurora_reset = '1';
---				end if;
-				counter_10_Hz := 0;
-				counter_1_Hz := counter_1_Hz + 1;
---				stupid_counter <= std_logic_vector(unsigned(stupid_counter) + 1);
-				if (spill_counter < spill_counter_maximum) then
-					spill_counter := spill_counter + 1;
-				else
-					spill_active <= '0';
-					fill_active  <= '1';
-					if (fill_counter < fill_counter_maximum) then
-						fill_counter := fill_counter + 1;
-					else
-						spill_active <= '1';
-						fill_active  <= '0';
-						spill_counter := 1;
-						fill_counter  := 1;
-					end if;
-				end if;
-			end if;
-		end if;
-	end process;
 -----------------------------------------------------------------------------
-	process(external_trigger_disable)
-	begin
-		if (external_trigger_disable = '0') then
-			gated_trigger <= external_triggers_ORed_together;
-		else
-			gated_trigger <= internal_trigger;
-		end if;
-	end process;
------------------------------------------------------------------------------
-	process (trigger_a_digitization_and_readout_event, transmit_always, trigger_acknowledge)
-	begin
-		if (trigger_acknowledge = '1') then
-			pulsed_trigger <= '0';
-		elsif (transmit_always = '1') then
-			pulsed_trigger <= '1';
-		elsif rising_edge(trigger_a_digitization_and_readout_event) then
-			pulsed_trigger <= '1';
-		end if;
-	end process;
------------------------------------------------------------------------------
-	process (clock_1MHz, global_reset)
-		variable trigger_acknowledge_counter : integer range 0 to 100 := 0;
-	begin
-		if (global_reset = '1') then
-			trigger_acknowledge <= '0';
-			trigger_acknowledge_counter := 0;
-		elsif rising_edge(clock_1MHz) then
-			if (internal_DONE_BUILDING_A_QUARTER_EVENT = '1') then
-				if (trigger_acknowledge_counter < 30) then
-					trigger_acknowledge <= '1';
-					trigger_acknowledge_counter := trigger_acknowledge_counter + 1;
-				else
-					trigger_acknowledge <= '0';
-				end if;
-			else
-				trigger_acknowledge <= '0';
-				trigger_acknowledge_counter := 0;
-			end if;
-		end if;
-	end process;
------------------------------------------------------------------------------
---	internal_acknowledge_start_event_transfer <= '0';
 	FR : entity work.fiber_readout
 	generic map (
 		CURRENT_PROTOCOL_FREEZE_DATE                => CURRENT_PROTOCOL_FREEZE_DATE,
@@ -325,7 +196,7 @@ begin
 		chipscope_ila                                           => open,
 		chipscope_vio_display                                   => fiber_readout_chipscope_vio_display,
 		chipscope_vio_buttons                                   => fiber_readout_chipscope_vio_buttons,
-		TRIGGER                                                 => pulsed_trigger,
+		TRIGGER                                                 => internal_TRIGGER,
 		DONE_BUILDING_A_QUARTER_EVENT                           => internal_DONE_BUILDING_A_QUARTER_EVENT,
 		INPUT_DATA_BUS                                          => internal_ASIC_DATA_BLOCKRAM_DATA_BUS,
 		INPUT_ADDRESS_BUS                                       => internal_ASIC_DATA_BLOCKRAM_ADDRESS_BUS,
@@ -345,17 +216,22 @@ begin
 -----------------------------------------------------------------------------
 	LVDS_SIMPLE_TRIGGER  : IBUFDS port map (I => REMOTE_SIMPLE_TRIGGER_P,  IB => REMOTE_SIMPLE_TRIGGER_N,  O => external_trigger_2_from_LVDS);
 	LVDS_ENCODED_TRIGGER : IBUFDS port map (I => REMOTE_ENCODED_TRIGGER_P, IB => REMOTE_ENCODED_TRIGGER_N, O => external_encoded_trigger_from_LVDS);
-
-	internal_trigger <= raw_5Hz_fake_trigger;
---	external_triggers_ORed_together <= external_trigger_1_from_monitor_header or external_trigger_2_from_LVDS;
---	external_triggers_ORed_together <= external_trigger_2_from_LVDS;
-	external_triggers_ORed_together <= external_trigger_1_from_monitor_header;
-	gated_fill_inactive <= fake_spill_structure_enable nand fill_active;
-	trigger_a_digitization_and_readout_event <= gated_fill_inactive and gated_trigger;
---	internal_PACKET_GENERATOR_ENABLE(1) <= '1';--trigger_a_digitization_and_readout_event or transmit_always;
---	internal_PACKET_GENERATOR_ENABLE(0) <= not transmit_disable;
---	trigger_a_digitization_and_readout_event
---	pulsed_trigger <= trigger_a_digitization_and_readout_event or transmit_always;
+	T : entity work.trigger
+	port map (
+		RESET                                    => global_reset,
+		CLOCK_1MHz                               => clock_1MHz,
+		CLOCK_1kHz                               => clock_1kHz,
+		external_trigger_from_monitor_header     => external_trigger_1_from_monitor_header,
+		external_simple_trigger_from_LVDS        => external_trigger_2_from_LVDS,
+		external_encoded_trigger_from_LVDS       => external_encoded_trigger_from_LVDS,
+		transmit_always                          => transmit_always,
+		fake_spill_structure_enable              => fake_spill_structure_enable,
+		external_trigger_disable                 => external_trigger_disable,
+		FAKE_SPILL_ACTIVE                        => spill_active,
+		raw_fake_trigger                         => raw_5Hz_fake_trigger,
+		TRIGGER                                  => internal_TRIGGER,
+		TRIGGER_ACKNOWLEDGE                      => internal_DONE_BUILDING_A_QUARTER_EVENT
+	);
 -----------------------------------------------------------------------------
 	LEDS(3 downto 0) <= internal_Aurora_RocketIO_GTP_MGT_101_status_LEDs;
 
@@ -367,29 +243,28 @@ begin
 	LEDS(8)  <= '0';--raw_5Hz_fake_trigger;
 	LEDS(9)  <= external_trigger_1_from_monitor_header;
 --	LEDS(10) <= external_triggers_ORed_together;
-	LEDS(10) <= pulsed_trigger;
-	LEDS(11) <= gated_trigger;
+	LEDS(10) <= internal_TRIGGER;
+--	LEDS(11) <= gated_trigger;
 
 --	LEDS(15 downto 12) <= internal_number_of_sent_events(3 downto 0);
-	LEDS(15 downto 12) <= (others => '0');
+	LEDS(15 downto 11) <= (others => '0');
 -----------------------------------------------------------------------------
 	MONITOR_HEADER_OUTPUT(0) <= '0';--Aurora_lane0_transmit_source_ready_active_low;
 	MONITOR_HEADER_OUTPUT(1) <= clock_1MHz;
 	MONITOR_HEADER_OUTPUT(2) <= clock_1kHz;
+
 	MONITOR_HEADER_OUTPUT(10 downto 3) <= (others => '0');
-	
 	MONITOR_HEADER_OUTPUT(11) <= external_trigger_2_from_LVDS;
 
-	MONITOR_HEADER_OUTPUT(12) <= pulsed_trigger;
-	MONITOR_HEADER_OUTPUT(13) <= raw_100Hz_fake_trigger;
+	MONITOR_HEADER_OUTPUT(12) <= internal_TRIGGER;
+	MONITOR_HEADER_OUTPUT(13) <= '0';
 	MONITOR_HEADER_OUTPUT(14) <= raw_5Hz_fake_trigger;
 	external_trigger_1_from_monitor_header <= MONITOR_HEADER_INPUT(15);
 -----------------------------------------------------------------------------
-
 	IBUFGDS_i_local  : IBUFGDS port map (I => board_clock_250MHz_P, IB => board_clock_250MHz_N, O => internal_clock_250MHz);
 	IBUFGDS_i_remote : IBUFGDS port map (I => REMOTE_CLOCK_P, IB => REMOTE_CLOCK_N, O => internal_clock_from_remote_source);
 	internal_clock_for_state_machine <= internal_clock_250MHz;
-
+-----------------------------------------------------------------------------
 	chipscope : entity work.chipscope
 	port map (
 		CLOCK       => internal_Aurora_78MHz_clock,
@@ -406,10 +281,9 @@ begin
 	chipscope_ila_trigger(255 downto 0) <= (others => '0');
 
 	chipscope_vio_display(0)             <= fiber_link_is_up;
-	chipscope_vio_display(1)             <= pulsed_trigger;
-	chipscope_vio_display(2)             <= trigger_acknowledge;
-	chipscope_vio_display(3)             <= spill_active;
-	chipscope_vio_display(255 downto 4)  <= (others => '0');
+	chipscope_vio_display(1)             <= internal_TRIGGER;
+	chipscope_vio_display(2)             <= spill_active;
+	chipscope_vio_display(255 downto 3)  <= (others => '0');
 
 	request_a_global_reset                             <= chipscope_vio_buttons(0);
 	request_a_fiber_link_reset                         <= chipscope_vio_buttons(1);
@@ -419,5 +293,5 @@ begin
 	fake_spill_structure_enable                        <= chipscope_vio_buttons(5);
 	should_not_automatically_try_to_keep_fiber_link_up <= chipscope_vio_buttons(6);
 	fake_button                                        <= chipscope_vio_buttons(7);
-
+-----------------------------------------------------------------------------
 end PDBF;
---
+++ iTOP-BLAB3A-boardstack/branches/pseudo-data_by_fiber/src/trigger.vhdl
@@ -0,0 +1,178 @@
+-- 2011-09 mza
+-----------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity trigger is
+	port (
+		RESET                                    : in    std_logic;
+		CLOCK_1MHz                               : in    std_logic;
+		CLOCK_1kHz                               : in    std_logic;
+		external_trigger_from_monitor_header     : in    std_logic;
+		external_simple_trigger_from_LVDS        : in    std_logic;
+		external_encoded_trigger_from_LVDS       : in    std_logic;
+		transmit_always                          : in    std_logic;
+		fake_spill_structure_enable              : in    std_logic;
+		external_trigger_disable                 : in    std_logic;
+		FAKE_SPILL_ACTIVE                        :   out std_logic;
+		raw_fake_trigger                         :   out std_logic;
+		TRIGGER                                  :   out std_logic;
+		TRIGGER_ACKNOWLEDGE                      : in    std_logic
+	);
+end trigger;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library UNISIM;
+use UNISIM.VComponents.all;
+
+architecture behavioral of trigger is
+	signal raw_500Hz_fake_trigger                   : std_logic := '0';
+	signal raw_100Hz_fake_trigger                   : std_logic := '0';
+	signal raw_5Hz_fake_trigger                     : std_logic := '0';
+	signal internal_raw_fake_trigger                : std_logic;
+	signal external_triggers_ORed_together          : std_logic;
+	signal gated_trigger                            : std_logic;
+	signal gated_fill_inactive                      : std_logic;
+	signal pulsed_trigger                           : std_logic;
+	signal trigger_a_digitization_and_readout_event : std_logic;
+	signal trigger_accepted                         : std_logic;
+	signal spill_active                             : std_logic;
+	signal fill_active                              : std_logic;
+-----------------------------------------------------------------------------
+begin
+-----------------------------------------------------------------------------
+	process (CLOCK_1MHz, RESET)
+		variable trigger_acknowledge_counter : integer range 0 to 100 := 0;
+	begin
+		if (RESET = '1') then
+			trigger_accepted <= '0';
+			trigger_acknowledge_counter := 0;
+		elsif rising_edge(CLOCK_1MHz) then
+			if (TRIGGER_ACKNOWLEDGE = '1') then
+				if (trigger_acknowledge_counter < 30) then
+					trigger_accepted <= '1';
+					trigger_acknowledge_counter := trigger_acknowledge_counter + 1;
+				else
+					trigger_accepted <= '0';
+				end if;
+			else
+				trigger_accepted <= '0';
+				trigger_acknowledge_counter := 0;
+			end if;
+		end if;
+	end process;
+-----------------------------------------------------------------------------
+	process(CLOCK_1kHz, RESET)
+		variable counter_1_kHz    : integer range 0 to 10 := 0;
+		variable counter_200_Hz   : integer range 0 to 10 := 0;
+		variable counter_100_Hz   : integer range 0 to 10 := 0;
+		variable counter_10_Hz    : integer range 0 to 10 := 0;
+		variable counter_1_Hz     : integer range 0 to 3600 := 0;
+		variable spill_counter         : integer range 0 to 60 := 1;
+		constant spill_counter_maximum : integer range 0 to 60 := 2;
+		variable fill_counter          : integer range 0 to 60 := 1;
+		constant fill_counter_maximum  : integer range 0 to 60 := 4;
+	begin
+		if (RESET = '1') then
+			spill_active <= '1';
+			fill_active  <= '0';
+			counter_1_kHz   := 0;
+			counter_200_Hz  := 0;
+			counter_100_Hz  := 0;
+			counter_10_Hz   := 0;
+			counter_1_Hz    := 0;
+			spill_counter   := 1;
+			fill_counter    := 1;
+			raw_5Hz_fake_trigger   <= '0';
+			raw_100Hz_fake_trigger <= '0';
+			raw_500Hz_fake_trigger <= '0';
+		elsif rising_edge(CLOCK_1kHz) then
+			-- evaluated once per millisecond
+			counter_1_kHz := counter_1_kHz + 1;
+			if (counter_1_kHz > 4) then
+				-- evaluated 200 times per second
+				counter_1_kHz := 0;
+				counter_200_Hz := counter_200_Hz + 1;
+				raw_100Hz_fake_trigger <= not raw_100Hz_fake_trigger;
+			end if;
+			if (counter_200_Hz > 1) then
+				-- evaluated 100 times per second
+				counter_200_Hz := 0;
+				counter_100_Hz := counter_100_Hz + 1;
+			end if;
+			if (counter_100_Hz > 9) then
+				-- evaluated 10 times per second
+				counter_100_Hz := 0;
+				counter_10_Hz := counter_10_Hz + 1;
+				raw_5Hz_fake_trigger <= not raw_5Hz_fake_trigger;
+			end if;
+--				raw_25Hz_fake_trigger <= not raw_25Hz_fake_trigger;
+			if (counter_10_Hz > 9) then
+				-- evaluated once per second
+--				if (fiber_link_is_up = '0') then
+--					pulsed_fiber_link_is_down = '1';
+--					request_an_aurora_reset = '1';
+--				end if;
+				counter_10_Hz := 0;
+				counter_1_Hz := counter_1_Hz + 1;
+--				stupid_counter <= std_logic_vector(unsigned(stupid_counter) + 1);
+				if (spill_counter < spill_counter_maximum) then
+					spill_counter := spill_counter + 1;
+				else
+					spill_active <= '0';
+					fill_active  <= '1';
+					if (fill_counter < fill_counter_maximum) then
+						fill_counter := fill_counter + 1;
+					else
+						spill_active <= '1';
+						fill_active  <= '0';
+						spill_counter := 1;
+						fill_counter  := 1;
+					end if;
+				end if;
+			end if;
+		end if;
+	end process;
+-----------------------------------------------------------------------------
+	process(external_trigger_disable)
+	begin
+		if (external_trigger_disable = '0') then
+			gated_trigger <= external_triggers_ORed_together;
+		else
+			gated_trigger <= internal_raw_fake_trigger;
+		end if;
+	end process;
+-----------------------------------------------------------------------------
+	process (trigger_a_digitization_and_readout_event, transmit_always, trigger_accepted)
+	begin
+		if (trigger_accepted = '1') then
+			pulsed_trigger <= '0';
+		elsif (transmit_always = '1') then
+			pulsed_trigger <= '1';
+		elsif rising_edge(trigger_a_digitization_and_readout_event) then
+			pulsed_trigger <= '1';
+		end if;
+	end process;
+-----------------------------------------------------------------------------
+	TRIGGER           <= pulsed_trigger;
+	FAKE_SPILL_ACTIVE <= spill_active;
+	raw_fake_trigger  <= internal_raw_fake_trigger;
+	internal_raw_fake_trigger <= raw_5Hz_fake_trigger;
+--	internal_trigger <= raw_5Hz_fake_trigger;
+--	external_triggers_ORed_together <= external_trigger_1_from_monitor_header or external_trigger_2_from_LVDS;
+--	external_triggers_ORed_together <= external_trigger_2_from_LVDS;
+	external_triggers_ORed_together <= external_trigger_from_monitor_header;
+	gated_fill_inactive <= fake_spill_structure_enable nand fill_active;
+	trigger_a_digitization_and_readout_event <= gated_fill_inactive and gated_trigger;
+--	internal_PACKET_GENERATOR_ENABLE(1) <= '1';--trigger_a_digitization_and_readout_event or transmit_always;
+--	internal_PACKET_GENERATOR_ENABLE(0) <= not transmit_disable;
+--	trigger_a_digitization_and_readout_event
+--	pulsed_trigger <= trigger_a_digitization_and_readout_event or transmit_always;
+-----------------------------------------------------------------------------
+end behavioral;
