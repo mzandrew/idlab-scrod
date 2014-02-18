fd4dee509c60460c0727ba1e7ffe989042f2b6b1
kirbybri <kirbybri@88b02a49-462a-2295-53a7-3445142cab38>
2014-02-18 22:47:19 +0000

    Additional usbInterface software updates not included in last commit.

--- SCROD-boardstack/KLM/T69UMB/usbInterface/setDACs.sh
+++ SCROD-boardstack/KLM/T69UMB/usbInterface/setDACs.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
 #Threshold Ch. 1(PCLK_1)
-./bin/target6Control_writeDacReg 0 0 2030
+./bin/target6Control_writeDacReg 0 0 2000
 #Threshold Ch. 2 (PCLK_2)
-#./bin/target6Control_writeDacReg 0 1 0
+./bin/target6Control_writeDacReg 0 1 2000
 #Threshold Ch. 3 (PCLK_3)
-#./bin/target6Control_writeDacReg 0 2 0
+./bin/target6Control_writeDacReg 0 2 2000
 #Threshold Ch. 4 (PCLK_4)
-#./bin/target6Control_writeDacReg 0 3 0
+./bin/target6Control_writeDacReg 0 3 2000
 #Threshold Ch. 5 (PCLK_5)
-#./bin/target6Control_writeDacReg 0 4 0
+./bin/target6Control_writeDacReg 0 4 2000
 #Threshold Ch. 6 (PCLK_6)
-#./bin/target6Control_writeDacReg 0 5 0
+./bin/target6Control_writeDacReg 0 5 2000
 #Threshold Ch. 7 (PCLK_7)
-#./bin/target6Control_writeDacReg 0 6 0
+./bin/target6Control_writeDacReg 0 6 2000
 #Threshold Ch. 8 (PCLK_8)
-#./bin/target6Control_writeDacReg 0 7 0
+./bin/target6Control_writeDacReg 0 7 2000
 #Threshold Ch. 9 (PCLK_9)
-#./bin/target6Control_writeDacReg 0 8 0
+./bin/target6Control_writeDacReg 0 8 2000
 #Threshold Ch. 10 (PCLK_10)
-#./bin/target6Control_writeDacReg 0 9 0
+./bin/target6Control_writeDacReg 0 9 2000
 #Threshold Ch. 11 (PCLK_11)
-#./bin/target6Control_writeDacReg 0 10 0
+./bin/target6Control_writeDacReg 0 10 2000
 #Threshold Ch. 12 (PCLK_12)
-#./bin/target6Control_writeDacReg 0 11 0
+./bin/target6Control_writeDacReg 0 11 2000
 #Threshold Ch. 13 (PCLK_13)
-#./bin/target6Control_writeDacReg 0 12 0
+./bin/target6Control_writeDacReg 0 12 2000
 #Threshold Ch. 14 (PCLK_14)
-#./bin/target6Control_writeDacReg 0 13 0
+./bin/target6Control_writeDacReg 0 13 2000
 #Threshold Ch. 15 (PCLK_15)
-#./bin/target6Control_writeDacReg 0 14 0
+./bin/target6Control_writeDacReg 0 14 2000
 #Threshold Ch. 16 (PCLK_16)
-#./bin/target6Control_writeDacReg 0 15 0
-
-#exit 0
+./bin/target6Control_writeDacReg 0 15 2000
 
 #ITBias (PCLK_17)
 ./bin/target6Control_writeDacReg 0 16 1300
@@ -46,7 +44,7 @@
 #TRGbias (PCLK_21)
 ./bin/target6Control_writeDacReg 0 20 1800
 #Wbias (PCLK_22)
-./bin/target6Control_writeDacReg 0 21 600
+./bin/target6Control_writeDacReg 0 21 700
 
 #SBbuff = 2047  (PCLK_38)
 ./bin/target6Control_writeDacReg 0 37 1400
@@ -76,9 +74,9 @@
 #DBbias (DAC buffer bias) (PCLK_48)
 ./bin/target6Control_writeDacReg 0 47 1500
 #ISEL (PCLK_49)
-./bin/target6Control_writeDacReg 0 48 2400
+./bin/target6Control_writeDacReg 0 48 2300
 #Vdischarge (PCLK_50)
-./bin/target6Control_writeDacReg 0 49 500
+./bin/target6Control_writeDacReg 0 49 800
 
 #PROBIAS (Vdly DAC bias) (PCLK_51)
 ./bin/target6Control_writeDacReg 0 50 1400
