6852c86296d6f1c2c770b495381a61a7a4cc9866
kirbybri <kirbybri@88b02a49-462a-2295-53a7-3445142cab38>
2014-06-12 09:51:37 +0000

    IRSC usb interface: added program to set SCROD ID on EEPROM, and improved the implementation of the program that takes triggered data

--- SCROD-boardstack/iTOP/IRS3C/usbInterface/src/lib/irs3BControlClass.cpp
+++ SCROD-boardstack/iTOP/IRS3C/usbInterface/src/lib/irs3BControlClass.cpp
@@ -600,6 +600,45 @@ int irs3BControlClass::i2c_read(unsigned int board_id, unsigned int regw, unsign
 }
 
 //read temperatures from board stack using I2C interface
+int irs3BControlClass::setSCRODEEPROM(unsigned short int board_id_to_write, unsigned short int board_revision_to_write){
+
+	int regValOut = 0;
+	std::cout << "Programming EEPROM " << std::hex << board_id_to_write << std::dec << std::endl;
+
+	unsigned short int board_id_low_mask = 0x00FF;
+        unsigned short int board_id_high_mask = 0xFF00;
+	unsigned int board_id = 0x00000000;  //Broadcast ID
+        //I2C command stack here
+        unsigned short int command_data[14]= {0x0100,  //1. start
+                                              0x00A0,  //2. setup send
+                                              0x02A0,  //   send addr + write
+                                              0x0000,  //3. setup send
+                                              0x0200,  //   send addr HIGH
+                                              0x0000,  //4. setup send
+                                              0x0200,  //   send addr LOW
+                                              (board_id_to_write & board_id_low_mask),  //5. setup send
+                                              0x0200 | (board_id_to_write & board_id_low_mask),  //   send data byte 1
+                                              ((board_id_to_write & board_id_high_mask) >> 8),  //6. setup send
+                                              0x0200 | ((board_id_to_write & board_id_high_mask) >> 8),  //   send data byte 2
+                                              board_revision_to_write,  //7. setup send
+                                              0x0200 | board_revision_to_write,  //   send data byte 3
+                                              0x1000}; //8. stop
+
+	//send write command over I2C bus as set of register writes
+	int regValReadback = 0;
+	for( int i = 0 ; i < 14 ; i++ ){
+		//try writing register, return if write fails
+		if(!registerWriteReadback(board_id, I2C_BUS_0_WR, command_data[i], regValReadback))
+			return 0;
+		usleep(50000); //5ms pause as I2C bus is slow
+		//check for ack
+		if(!registerRead(board_id, I2C_BUS_0_RD, regValReadback))
+			return 0;
+	}
+	return 1;
+}
+
+//read temperatures from board stack using I2C interface
 int irs3BControlClass::readTemperatures(unsigned int board_id){
 
 	int regValOut = 0;
