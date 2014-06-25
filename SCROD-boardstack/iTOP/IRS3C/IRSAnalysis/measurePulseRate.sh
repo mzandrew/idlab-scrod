26221599e52bfb19695483ecb94e437793463f74
kirbybri <kirbybri@88b02a49-462a-2295-53a7-3445142cab38>
2014-06-25 03:35:14 +0000

    IRS3C IRSAnalysis: Added script used to quickly measure dark noise rate

---
+++ SCROD-boardstack/iTOP/IRS3C/IRSAnalysis/measurePulseRate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Mandatory - Set binary directory
+BINDIR=./
+
+#set COPPER board working and data directories
+WORKDIR=/home/copper/crt_data/20140624_coldstart/
+DATADIR=/export/home/copper/crt_data/20140624_coldstart/data/
+NUMEVENTS=2000
+
+#Create script to send to COPPER for taking hardware triggers
+rm copperCommands_hwt.sh
+echo -e "#/bin/bash" >> copperCommands_hwt.sh
+echo -e "cd "${WORKDIR} >> copperCommands_hwt.sh
+echo -e "python ~/bin/crtdaq-hwt-run.py -n "${NUMEVENTS} >> copperCommands_hwt.sh
+echo -e "exit" >> copperCommands_hwt.sh
+
+#send command to COPPER to take hardware triggers
+cat copperCommands_hwt.sh | ssh 192.168.1.203
+
+#Get hardware trigger file name
+DATAFILE=$( ls -t ${DATADIR} | head -n2 | grep hwt | grep dat )
+echo $DATAFILE
+
+#process data file with pedestal file
+./processIRSDataNoPedNoCmc.sh ${DATADIR}${DATAFILE}
+
+#get summary tree file name
+TREEFILE=$( ls -t | head -n1 | grep summary )
+
+#run analysis program on summary tree file - requires # of events as argument for rate calc.
+./topDataClass_simpleDistributions ${TREEFILE} ${NUMEVENTS}
