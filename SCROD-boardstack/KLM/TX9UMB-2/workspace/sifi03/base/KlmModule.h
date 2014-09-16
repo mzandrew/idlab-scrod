14c2a3294b8c4ab0c201e41a711931258f43ef29
isar.mostafanezhad <isar.mostafanezhad@88b02a49-462a-2295-53a7-3445142cab38>
2014-09-16 02:43:01 +0000

    Consolidated the usbInterface and software folders into one workspace folder. It is a eclipse Kepler workspace env.

---
+++ SCROD-boardstack/KLM/TX9UMB-2/workspace/sifi03/base/KlmModule.h
@@ -0,0 +1,92 @@
+#ifndef INCLUDE_KLM_MODULE
+#define INCLUDE_KLM_MODULE
+
+#include <queue>
+
+#include "base/DetectorInterface.h"
+#include "base/defines.h"
+#include "base/ScrodPacket.h"
+#include "base/ObjectSync.h"
+
+
+class KlmModule
+	/// Class for high lavel KLM module manipulation.
+{
+public:
+	
+	typedef enum { hw_trigger, sw_trigger } TrgType_t;
+
+	KlmModule(DetectorInterface* interface);
+		/// Constructor.
+	
+	virtual ~KlmModule();
+		/// Destructor.
+
+	// primitive functions
+	
+	void write_register(scrod_address address, scrod_register value, bool verify = true);
+		/// Writes a value to the desired register.
+		
+	scrod_word read_register(scrod_address address);
+		/// Reads the value of the desired register.
+		
+	void write_ASIC_register(uint8_t card, uint8_t address, uint16_t value);
+		/// Writes a value to the ASIC register.
+	
+	void setDeviceID(module_id id);
+		/// Sets the ID of the underlying device.
+		
+	int send_trigger(KlmModule::TrgType_t trigger);
+		/// Sends the trigger.
+		
+	ScrodPacket* read_packet() const;
+		/// Reads the next packet and return it.
+		
+	bool start(ObjectSync<ScrodPacket>*	data_drain);
+		/// Runs this module.
+		
+	void stop();
+		/// Stops this module.
+		
+	void wait_end(std::ostream& output);
+		/// Waits for the end of execution.
+
+	static void* pt_starter(void* module);
+		/// Starts the module.
+	
+protected:
+
+	void run();
+		/// Runs this module.		
+
+	bool should_continue() const;
+		/// Should the thread continue.
+		
+private:
+
+	KlmModule();
+		/// Constructor.
+	
+private:
+
+	DetectorInterface*	_interface;
+	scrod_word			_packet_id;
+	
+	//std::queue<ScrodPacket*>	_packet_queue;
+	bool				_run;
+	
+	pthread_t		_thread;
+	pthread_attr_t	_thread_att;
+	ObjectSync<ScrodPacket>*	_data_drain;
+};
+
+inline void KlmModule::setDeviceID(module_id id)
+{
+	_interface->setDeviceID(id);
+}
+
+inline bool KlmModule::should_continue() const
+{
+	return _run;
+}
+#endif
---
+++ SCROD-boardstack/KLM/TX9UMB-2/workspace/usbInterface/src/target6Control_takeData.cpp
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h> 
+#include "target6ControlClass.h"
+
+#include <fstream>
+
+#include <TGraph.h>
+#include <TH1.h>
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TF1.h"
+#include "TMath.h"
+#include "TFile.h"
+#include "TGraphErrors.h"
+
+//global TApplication object declared here for simplicity
+TApplication *theApp;
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	if (argc != 3){
+    		std::cout << "wrong number of arguments: usage ./target6Control_takeData <number events> <trigger type: software = 0 or ASIC trigger = 1>" << std::endl;
+    		return 0;
+  	}
+
+	//get event #
+	int numEvents = atoi(argv[1]);
+	if( numEvents <= 0 ){
+		std::cout << "Invalid number of events, exiting" << std::endl;
+		return 0;
+	}
+
+	//get trigger type
+	int trigType = atoi(argv[2]);
+	if( trigType != 0 && trigType != 1 ){
+		std::cout << "Invalid trigger type, exiting" << std::endl;
+		return 0;
+	}
+
+	//define application object
+	theApp = new TApplication("App", &argc, argv);
+
+	int regValReadback;
+
+	//create target6 interface object
+	target6ControlClass *control = new target6ControlClass();
+
+	//initialize USB interface - Mandatory
+	control->initializeUSBInterface();
+
+	//define the SCROD ID for board stack, hardcoded for now
+	unsigned int board_id = 0;
+
+	//clear data buffer
+	control->clearDataBuffer();
+
+	//make simple sample histogram
+	TCanvas *c0 = new TCanvas("c0","c0",1300,800);
+	TH1F *hSampDist = new TH1F("hSampDist","",110,3000,4100);
+
+	//Initialize
+	control->registerWriteReadback(board_id, 11, 0, regValReadback); //stop sampling
+	control->registerWriteReadback(board_id, 11, 1, regValReadback); //Start sampling
+	usleep(20);
+	control->registerWriteReadback(board_id, 11, 0, regValReadback); //stop sampling
+	usleep(10);
+
+	control->registerWriteReadback(board_id, 20, 0, regValReadback); //Digitization OFF
+	control->registerWriteReadback(board_id, 30, 0, regValReadback); //Serial readout OFF
+	control->registerWriteReadback(board_id, 50, 0, regValReadback); //readout control start is 0
+	control->registerWrite(board_id, 44, 0, regValReadback); //Stop event builder
+	control->registerWrite(board_id, 45, 1, regValReadback); //Reset Event builder
+	control->registerWrite(board_id, 45, 0, regValReadback); //Reset Event builder
+	//control->registerWriteReadback(board_id, 51, 0x3FF, regValReadback); //enable ASICs for readout
+	control->registerWriteReadback(board_id, 51, 0x200, regValReadback); //enable ASICs for readout: 7/12/14: IM: changed such that only DC10 is measured
+	control->registerWriteReadback(board_id, 52, 0, regValReadback); //veto hardware triggers
+	control->registerWriteReadback(board_id, 53, 8, regValReadback); //set trigger delay
+	control->registerWriteReadback(board_id, 54, 100, regValReadback); //set digitization window offset: internal_CMDREG_READCTRL_dig_offset
+	control->registerWriteReadback(board_id, 55, 1, regValReadback); //reset readout
+	control->registerWriteReadback(board_id, 55, 0, regValReadback); //reset readout
+	control->registerWriteReadback(board_id, 56, 0, regValReadback); //select readout control module signals
+	control->registerWriteReadback(board_id, 57, 16, regValReadback); //set # of windows to read: internal_READCTRL_win_num_to_read
+	control->registerWrite(board_id, 58, 0, regValReadback); //reset packet request
+	control->registerWrite(board_id, 72, 0x3FF, regValReadback); //enable trigger bits
+	control->registerWrite(board_id, 61, 0xD00, regValReadback); //ramp length- working on 40us ish
+
+	//define output file		
+	ofstream dataFile;
+  	dataFile.open ("output_target6Control_takeData.dat", ios::out | ios::binary );
+
+	unsigned int eventdatabuf[65536];
+	int eventdataSize = 0;
+	int numIter = 0;
+
+	char ct = 0;
+	//while(ct != 'Q'){
+	for( int numEv = 0 ; numEv < numEvents ; numEv++ ){
+		if( numEv % 10 == 0 )
+			std::cout << "Event # " << numEv << std::endl;
+		//do software trigger
+		if(trigType == 0){
+			control->sendTrigger(board_id,0);
+			usleep(50);
+			int cnt1,cnt2;
+			control->registerRead(board_id,256+5,cnt1);
+			control->registerRead(board_id,256+30,cnt2);
+			cout<<endl<<"SMP_Latch: "<<cnt1<<", Dig win start: "<<cnt2;
+		}
+		//do harware trigger, presumably trigger will occur shortly after hardware veto is disable
+		else{
+			control->sendTrigger(board_id,1);
+			usleep(50);
+			//std::cout << "Send trigger, then enter character" << std::endl;
+			//std::cin >> ct;
+		}
+
+		int first = 1;
+		int numSmall = 0;
+		numIter = 0;
+		while( (eventdataSize > 100 || numSmall < 3 ) && numIter < 25 ){
+		//while( numIter < 4){
+			//delay, just in case readout is still in progress
+			
+			//toggle continue bit
+			control->continueReadout(board_id);
+
+			//usleep(100);
+	
+			//parse the data packet, look for event packets
+			control->readPacketFromUSBFifo( eventdatabuf, 65536, eventdataSize );
+		
+			//increment iterate count
+			numIter++;
+
+			//save data to file
+			//myfile.write(reinterpret_cast<char*>(&eventdatabuf), eventdataSize*sizeof(unsigned int));
+			if( eventdataSize > 100 ){
+				first = 0;
+				numSmall = 0;
+				control->writeEventToFile(eventdatabuf, eventdataSize, dataFile );
+			}
+			else
+				numSmall++;
+		} //end while loop
+		//if( first == 0 )
+		//	std::cout << "\tRecorded waveform data" << std::endl;
+	}//end event loop
+
+	//reset readout
+	control->registerWriteReadback(board_id, 50, 0, regValReadback); //readout control start is 0
+	control->registerWriteReadback(board_id, 52, 0, regValReadback); //veto hardware triggers
+	control->registerWrite(board_id, 55, 1, regValReadback); //make sure readout is reset
+	control->registerWrite(board_id, 55, 0, regValReadback); //make sure readout is reset
+
+	//close output file
+  	dataFile.close();
+
+	//close USB interface
+        control->closeUSBInterface();
+
+	//delete target6 interface object
+	delete control;
+
+	return 1;
+}	
